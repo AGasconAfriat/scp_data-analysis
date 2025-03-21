01d49c458387edd6b70f0df0605e03776a7c4044
AGasconAfriat <47426288+AGasconAfriat@users.noreply.github.com>
2025-03-21 14:13:44 -0400

    Using scp6999augmented.csv

--- dashboard.py
+++ dashboard.py
@@ -15,30 +15,10 @@ def contains_count(string, elements):
     for element in elements:
         count += string.count(element)
     return count
-def get_class(text):
-    x = re.findall(r"\n (\w*) Class: (\w*) \n", text)
-    if x :
-        return(list(x[0]))
-    else:
-        return ["None", "None"]
-def get_class_type(text):
-    return get_class(text)[0]
-def get_class_spec(text):
-    return get_class(text)[1]
-def get_series(scp_code):
-    num = int(scp_code[4:])
-    return math.ceil(num/1000)
 
 pd.set_option('display.max_colwidth', None)
 
-#TODO set back to download after development
-#path = kagglehub.dataset_download("czzzzzzz/scp1to7")
-#df = pd.read_csv(f"{path}/scp6999.csv")
-df = pd.read_csv("scp6999.csv")
-
-df["class type"] = df["text"].apply(get_class_type)
-df["class"] = df["text"].apply(get_class_spec)
-df["series"] = df["code"].apply(get_series)
+df = pd.read_csv("scp6999augmented.csv")
 
 dbc_css = (
     "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.1/dbc.min.css"
